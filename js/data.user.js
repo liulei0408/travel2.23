11cfb27c9dd7a70dd5ca1efdbe9bfc721ba897d3
liulei0408 <1159439271@qq.com>
2017-02-23 14:53:37 +0800

    milk

---
+++ js/data.user.js
@@ -0,0 +1,22 @@
+var r=[{'name':'liu','password':'123','pass':'123'},
+    {'name':'1','password':'1','pass':'1'},
+    {'name':'2','password':'2','pass':'2'},
+    {'name':'3','password':'3','pass':'3'}]
+
+var dancers=[
+    {'pic':'images/video-1.jpg',
+     'w':'爵士舞',
+     'e':'爵士舞爵士舞爵士舞爵士舞爵士舞爵士舞爵爵士舞爵士舞爵士舞爵士舞爵士舞爵士舞爵士舞爵'
+    },
+    {'pic':'images/video.jpg',
+        'w':'日韩舞',
+        'e':'爵士舞爵士舞爵士舞爵士舞爵士舞爵士舞爵爵士舞爵士舞爵士舞爵士舞爵士舞爵士舞爵士舞爵'
+    }
+]
+ var downs=[{'id':'downs1',
+     'up':'爵士舞'
+ },
+     {'id':'downs2',
+         'up':'日韩舞'
+     }
+ ]
